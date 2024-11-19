00031de91dbe1a2efcafcb35a5b3dae204c47f93
SypniewskiMarcin <sypniewskimarcin2k@gmail.com>
2024-11-19 03:44:31 +0100

    Przed dodaniem panelu admina oraz ról do kodu

---
+++ .history/src/components/AdminPanel_20241119033745.js
@@ -0,0 +1,14 @@
+// src/components/AdminPanel.js
+import React from 'react';
+
+const AdminPanel = () => {
+    return (
+        <div>
+            <h2>Panel Administracyjny</h2>
+            <p>Witaj w panelu administracyjnym!</p>
+            {/* Dodaj więcej funkcji administracyjnych tutaj */}
+        </div>
+    );
+};
+
+export default AdminPanel;
